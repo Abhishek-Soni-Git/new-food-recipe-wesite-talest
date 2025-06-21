60f9c0ae6c43f66338a8ce698b0da431f1fe1568
Abhishek Soni <soniabhishek4567@gmail.com>
2025-06-21 16:39:32 +0530

    b

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,6 +1,6 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import multer from 'multer';
 import path from 'path';
 import { OAuth2Client } from 'google-auth-library';
